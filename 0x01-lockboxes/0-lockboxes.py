562bc132a66680f1165faeace0ae9152f8ec9384
kwesimain <opokulive@gmail.com>
2024-03-16 03:05:55 +0000

    comm3

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,26 @@
+#!/usr/bin/python3
+"""Lockboxes Contains method that finds the keys to
+open other lockboxes
+"""
 def canUnlockAll(boxes):
-    # Create a set to keep track of unlocked boxes
-    unlocked = set([0])
-    # Create a queue for breadth-first search, starting with box 0
-    queue = [0]
-    
-    while queue:
-        # Get the current box from the queue
-        current_box = queue.pop(0)
-        # Iterate over the keys in the current box
-        for key in boxes[current_box]:
-            # If the key corresponds to a box we haven't unlocked yet, unlock it and add it to the queue
-            if key < len(boxes) and key not in unlocked:
-                unlocked.add(key)
-                queue.append(key)
-    
-    # If the number of unlocked boxes equals the total number of boxes, return True
-    return len(unlocked) == len(boxes)
+    """A function that checks if all boxes can be opened"""
+    keys = []  # List to store unique keys to unlock boxes
+    length = len(boxes)
+    idx = 0
+
+    # Iterate over the boxes
+    for i in boxes:
+        # Iterate over numbers in each boxes
+        for j in i:
+            # Store unique numbers from boxes as keys
+            if j not in keys and j != 0 and j != idx:
+                keys.append(j)
+        idx += 1
+    # Sort the keys in ascending order
+    keys = sorted(keys)
+    # Iterate over the range of the length of the key, and make sure that each
+    # box is opened by the keys.
+    for k in range(1, length):
+        if k != keys[k - 1]:
+            return False
+    return True
